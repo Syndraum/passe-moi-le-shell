1afeded2bfb71fe8b775db48a2160ba66164253f
Syndraum <roms2111@gmail.com>
2020-03-28 17:50:16 +0100

    Delete unused variable

--- libft/put/ft_putstr_fd.c
+++ libft/put/ft_putstr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mchardin <mchardin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: syndraum <syndraum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 15:14:41 by mchardin          #+#    #+#             */
-/*   Updated: 2019/11/30 17:46:50 by mchardin         ###   ########.fr       */
+/*   Updated: 2020/03/28 17:13:38 by syndraum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,6 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	int i;
-
-	i = 0;
 	if (!s)
 		return ;
 	write(fd, s, ft_strlen(s));
