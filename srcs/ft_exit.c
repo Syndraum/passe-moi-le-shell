1d781b1fe465618011337d3632264dfdc8524133
Syndraun <roms2111@gmail.com>
2020-04-30 13:50:33 +0200

    exit check

--- srcs/ft_exit.c
+++ srcs/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/28 15:05:53 by mchardin          #+#    #+#             */
-/*   Updated: 2020/04/30 12:24:59 by user42           ###   ########.fr       */
+/*   Updated: 2020/04/30 13:49:40 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,40 @@ void		exit_error(t_shell *shell, char *fonction)
 	exit(1);
 }
 
-void		exit_end(t_shell *shell)
+void		to_exit(t_shell *shell)
 {
-	ft_putstr_fd("exit\n", shell->fd_line);
 	close_all(shell);
 	free_all(shell);
 	exit(shell->stop);
+}
+
+void		exit_end(t_shell *shell)
+{
+	int len;
+	int error;
+
+	ft_putstr_fd("exit\n", shell->fd_line);
+	if ((len = ft_strslen(shell->tab)) > 1)
+	{
+		if (!(error = ft_atoi(shell->tab[1])))
+		{
+			shell->stop = 2;
+			print_error(shell, "exit ", "numeric argument needed");
+			to_exit(shell);
+		}
+		else if (len > 2)
+		{
+			print_error(shell, "exit", "too many argument");
+			shell->stop = 1;
+		}
+		else
+		{
+			shell->stop = error;
+			to_exit(shell);
+		}
+	}
+	else
+		to_exit(shell);
 }
 
 void		print_error(t_shell *shell, char *s2, char *s1)
